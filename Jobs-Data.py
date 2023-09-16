47ff5021f81328c63dd19e5a27c64b7d65ffc729
Zahraa Sleem <zahraasleem03@gmail.com>
2023-09-16 22:53:48 +0300

    Got job description and skills in addition to preffered things

--- Jobs-Data.py
+++ Jobs-Data.py
@@ -4,76 +4,129 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+from selenium.webdriver.common.by import By
 
-def get_job_info(job_id, driver):
+def get_job_info(driver, job_id):
     print("Job Id", job_id)
-    # Getting the url
+    
+    # Getting the URL
     job_url = f'https://www.bayt.com/en/international/jobs/?jobId={job_id}'
     driver.get(job_url)
-
-    # Accessing the parent element
+    
+    job_description = ""  # Initialize the job description string
+    skills = []  # Initialize the skills list
+    
     while True:
         try:
-            parent_element = driver.find_element(By.XPATH, '//*[@id="view_inner"]/div/div[2]/dl[1]')
+            # Find the starting point for job description
+            job_description_start = driver.find_element(By.XPATH, '//*[@id="view_inner"]/div/div[2]/h2[2]')
             break
         except Exception as e:
             print(f"Error: {e}")
             pass
-
+        
+    # Iterate through siblings
+    current_element = job_description_start
+    while current_element is not None:
+        # Check if the current element is a <p> tag
+        if current_element.tag_name == "p":
+            # Add the text of the <p> tag to the job description
+            job_description += current_element.text
+            # Check if the <p> tag contains a <br>, add a newline character
+            if "<br>" in current_element.get_attribute("innerHTML"):
+                job_description += "\n"
+        
+        # Check if we've reached the "Skills" section
+        if "Skills" in current_element.text:
+            skills_start = current_element
+            break
+        
+        # Move to the next sibling
+        current_element = current_element.find_element(By.XPATH, "./following-sibling::*")
+    
+    # Iterate through siblings starting from the "Skills" section
+    current_element = skills_start
+    while current_element is not None:
+        # Check if the current element is a <p> tag
+        if current_element.tag_name == "p":
+            # Check if the <p> tag contains a <br>, add nothing
+            if "<br>" not in current_element.get_attribute("innerHTML"):
+                # Add the text of the <p> tag to the skills list
+                skills.append(current_element.text)
+        elif current_element.tag_name == "ul":
+            skill_items = current_element.find_elements(By.TAG_NAME, "li")
+            for item in skill_items:
+                if "<br>" not in item.get_attribute("innerHTML"):
+                    skills.append(item.text)
+            
+        # Check if we've reached the "Job Details" section
+        if "Job Details" in current_element.text:
+            break
+        
+        # Move to the next sibling
+        current_element = current_element.find_element(By.XPATH, "./following-sibling::*")
+    
+    # Accessing the parent element for details
+    while True:
+        try:
+            parent_element_details = driver.find_element(By.XPATH, '//*[@id="view_inner"]/div/div[2]/dl[1]')
+            break
+        except Exception as e:
+            print(f"Error: {e}")
+            pass
+    
     # Find child elements within the <dl> element
-    child_elements = parent_element.find_elements(By.XPATH, ".//*")
-
-    # Create a dictionary to store the extracted information
-    info_dict = {}
-
-    # Loop through child elements and extract the data
-    for child in child_elements:
+    child_elements_details = parent_element_details.find_elements(By.XPATH, ".//*")
+    
+    # Create a dictionary to store the extracted information concerning details
+    info_dict_general = {}
+    
+    for child in child_elements_details:
         try:
             # Extract the <dt> and <dd> elements
             dt_element = child.find_element(By.TAG_NAME, "dt")
             dd_element = child.find_element(By.TAG_NAME, "dd")
 
             # Get the text content of <dt> and <dd> elements and add it to the dictionary
-            info_dict[dt_element.text] = dd_element.text
+            info_dict_general[dt_element.text] = dd_element.text
         except:
             pass
+    
+    # Accessing the parent element for preferred
+    while True:
+        try:
+            parent_element_preffered = driver.find_element(By.XPATH, '//*[@id="view_inner"]/div/div[2]/dl[2]')
+            break
+        except Exception as e:
+            print(f"Error: {e}")
+            pass
+    
+    # Find child elements within the <dl> element
+    child_elements_preffered = parent_element_preffered.find_elements(By.XPATH, ".//*")
+    
+    # Create a dictionary to store the extracted information concerning preferred
+    info_dict_preffered = {}
+    
+    for child in child_elements_preffered:
+        try:
+            # Extract the <dt> and <dd> elements
+            dt_element = child.find_element(By.TAG_NAME, "dt")
+            dd_element = child.find_element(By.TAG_NAME, "dd")
+
+            # Get the text content of <dt> and <dd> elements and add it to the dictionary
+            info_dict_preffered[dt_element.text] = dd_element.text
+        except:
+            pass
+    
     # Add the job data dictionary to the list
-    job_data = {"Job ID": job_id, "Data": info_dict}
+    job_data = {"Job ID": job_id,"Description":job_description, "Skills":skills,"Preferred":info_dict_preffered, "Data": info_dict_general}
 
-    # Open the JSON file in append mode and write the job data
-    with open("job_data.json", "a", encoding="utf-8") as json_file:
-        json.dump(job_data, json_file, ensure_ascii=False,indent=4)
-        json_file.write(",")
-
-# Read the JSON file as a list of job IDs
-with open("all_job_ids.json", "r") as json_file:
-    job_ids = json.load(json_file)
-
-# Initialize jobsloaded as an empty list
-jobsloaded = []
-
-# Check if the "job_data.json" file exists and is not empty
-try:
-    with open('job_data.json', 'r') as input_file:
-        for line in input_file:
-            if line.strip():  # Check if the line is not empty
-                job_data = json.loads(line)
-                jobsloaded.append(job_data)
-except (FileNotFoundError, json.decoder.JSONDecodeError):
-    # Handle the case where the file doesn't exist or is empty
-    pass
-
-# Extract existing Job IDs from loaded data
-existing_ids = [job['Job ID'] for job in jobsloaded]
-
-# Find new Job IDs that haven't been loaded
-new_ids = [job_id for job_id in job_ids if job_id not in existing_ids]
+    print(job_data)
+    
+    
 
 # Initialize the WebDriver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
-# Iterate over new job IDs and get job information
-[get_job_info(id,driver) for id in new_ids]
-
+get_job_info(driver,69872937)
 # Quit the WebDriver after all new jobs have been processed
 driver.quit()
