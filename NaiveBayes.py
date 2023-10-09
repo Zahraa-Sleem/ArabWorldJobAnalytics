1e632c6fb42a8d6044ee0e901c3c7a65f5e7ed38
Zahraa Sleem <zahraasleem03@gmail.com>
2023-10-09 21:09:35 +0300

    Multinomial used

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,11 +1,10 @@
 import os
 import json
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
-import category_encoders as ce
-from sklearn.preprocessing import RobustScaler
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score
+
 
 def make_data_set(folder_name):
     directory_path = folder_name
@@ -27,48 +26,23 @@ def make_data_set(folder_name):
 
 data=make_data_set("data_store")
 
-Training_testing=[entry for entry in data if entry["Job Title"] != "Unspecified"]
+filtered_data = [entry for entry in data if entry.get("Job Title") != "Unspecified"]
 
-df = pd.DataFrame(Training_testing)
+# Separate features (job descriptions) and labels (job titles)
+X = [entry["Description"] for entry in filtered_data]
+y = [entry["Job Title"] for entry in filtered_data]
 
-X = df['Description']
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
 
-y = df['Job Title']
+vectorizer = CountVectorizer()
+X_train_vec = vectorizer.fit_transform(X_train)
+X_test_vec = vectorizer.transform(X_test)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+clf = MultinomialNB()
+clf.fit(X_train_vec, y_train)
 
-encoder = ce.OneHotEncoder(cols=['Description'])
+accuracy = clf.score(X_test_vec, y_test)
+print(f"Accuracy: {accuracy}")
 
-X_train = encoder.fit_transform(X_train)
 
-X_test = encoder.transform(X_test)
 
-cols = X_train.columns
-
-scaler = RobustScaler()
-
-X_train = scaler.fit_transform(X_train)
-
-X_test = scaler.transform(X_test)
-
-X_train = pd.DataFrame(X_train, columns=[cols])
-
-X_test = pd.DataFrame(X_test, columns=[cols])
-
-gnb = GaussianNB()
-
-gnb.fit(X_train, y_train)
-
-y_pred = gnb.predict(X_test)
-
-print('Model accuracy score: {0:0.4f}'. format(accuracy_score(y_test, y_pred)))
-
-y_pred_train = gnb.predict(X_train)
-
-print('Training-set accuracy score: {0:0.4f}'. format(accuracy_score(y_train, y_pred_train)))
-
-# print the scores on training and test set
-
-print('Training set score: {:.4f}'.format(gnb.score(X_train, y_train)))
-
-print('Test set score: {:.4f}'.format(gnb.score(X_test, y_test)))
